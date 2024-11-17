98de326a1cfd7bee4381cb3e9efaf88f0fd100c5
navinAce <navinraj99305@gmail.com>
2024-11-18 01:51:08 +0530

    Remove commented-out database connection code and retain production configuration

--- Server/db/db.js
+++ Server/db/db.js
@@ -1,26 +1,3 @@
-// import mysql2 from 'mysql2';
-
-// const connection = mysql2.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME 
-// });
-
-// connection.connect((err) => {
-//     if (err) {
-//         console.error('Error connecting to the database:', err.stack);
-//         return;
-//     }
-//     console.log('Connected to the MySQL server');
-// });
-
-
-// export default connection;
-
-
-//production
-
 import mysql2 from 'mysql2';
 
 const connection = mysql2.createConnection({
