9bbdf5183392e91fc6da22c73f2934f84e372611
navinAce <navinraj99305@gmail.com>
2024-11-18 01:07:07 +0530

    Update database connection settings for production environment

--- Server/db/db.js
+++ Server/db/db.js
@@ -1,10 +1,34 @@
+// import mysql2 from 'mysql2';
+
+// const connection = mysql2.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME 
+// });
+
+// connection.connect((err) => {
+//     if (err) {
+//         console.error('Error connecting to the database:', err.stack);
+//         return;
+//     }
+//     console.log('Connected to the MySQL server');
+// });
+
+
+// export default connection;
+
+
+//production
+
 import mysql2 from 'mysql2';
 
 const connection = mysql2.createConnection({
-    host: 'localhost',
-    user: 'root',
+    host: 'b0oynhti3dmrn9dugkyb-mysql.services.clever-cloud.com',
+    user: 'uaz00jqovdzmb5we',
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME 
+    database: process.env.DB_NAME,
+    port: 3306
 });
 
 connection.connect((err) => {
