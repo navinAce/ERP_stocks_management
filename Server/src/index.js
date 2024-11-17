3dd6c5cd4e810bf9d949273415a9c7052f6eef15
navinAce <navinraj99305@gmail.com>
2024-11-17 18:13:43 +0530

    success

--- Server/src/index.js
+++ Server/src/index.js
@@ -11,6 +11,10 @@ connection.on('error', (err) => {
 
 const PORT=process.env.PORT || 3000;
 
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
